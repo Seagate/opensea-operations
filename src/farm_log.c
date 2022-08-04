29df6dcf1907d44f7ff0bb8f114561e382adf235
pranali-tirkhunde <pranali.tirkhunde@seagate.com>
2022-08-04 15:27:18 +0530

    feat: Adding support to pull FARM combined Log
    Adding header file and source file in order to support the operations for pulling FARM Combined Log. This operation would be supported in Linux, VS-2017 and VS-2019 after the current commit.

---
+++ src/farm_log.c
@@ -0,0 +1,20 @@
+//
+// Do NOT modify or remove this copyright and license
+//
+// Copyright (c) 2012-2022 Seagate Technology LLC and/or its Affiliates, All Rights Reserved
+//
+// This software is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// ******************************************************************************************
+// 
+// \file farm_log.c
+// \brief This file defines the functions related to FARM Log
+
+#include "farm_log.h"
+
+int pull_FARM_Combined_Log(tDevice *device, const char * const filePath, uint32_t transferSizeBytes, uint32_t delayTime)
+{
+	return SUCCESS;
+}
