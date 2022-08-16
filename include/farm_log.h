44bc6ab534a954ff6fa482751812bce5e0eb4e50
Nidhi Sharma <nidhi.sharma@seagate.com>
2022-08-17 00:35:10 +0530

    Quick : Adding implementation for header, dataset entry subheader, and pulling "Current Farm", "Factory Farm" and "Saved Farm" for SATA drive.

--- include/farm_log.h
+++ include/farm_log.h
@@ -20,23 +20,23 @@ extern "C"
 {
 #endif
 
-	//-----------------------------------------------------------------------------
-	//
-	//  pull_FARM_Combined_Log(tDevice *device, const char * const filePath);
-	//
-	//! \brief   Description: This function pulls the Seagate Combined FARM log. This Log is a combination of all
-	//!						  FARM Log Subpages.
-	//
-	//  Entry:
-	//!   \param[in] device = poiner to a valid device structure with a device handle
-	//!   \param[in] filePath = pointer to the path where this log should be generated. Use NULL for current working dir.
-	//!   \param[in] transferSizeBytes = OPTIONAL. If set to zero, this is ignored. 
-	//!   \param[in] delayTime = delay time between commands to control performance.
-	//  Exit:
-	//!   \return SUCCESS = everything worked, !SUCCESS means something went wrong
-	//
-	//-----------------------------------------------------------------------------
-	OPENSEA_OPERATIONS_API int pull_FARM_Combined_Log(tDevice *device, const char * const filePath, uint32_t transferSizeBytes, uint32_t delayTime);
+    //-----------------------------------------------------------------------------
+    //
+    //  pull_FARM_Combined_Log(tDevice *device, const char * const filePath);
+    //
+    //! \brief   Description: This function pulls the Seagate Combined FARM log. This Log is a combination of all
+    //!						  FARM Log Subpages.
+    //
+    //  Entry:
+    //!   \param[in] device = poiner to a valid device structure with a device handle
+    //!   \param[in] filePath = pointer to the path where this log should be generated. Use NULL for current working dir.
+    //!   \param[in] transferSizeBytes = OPTIONAL. If set to zero, this is ignored. 
+    //!   \param[in] delayTime = delay time between commands to control performance.
+    //  Exit:
+    //!   \return SUCCESS = everything worked, !SUCCESS means something went wrong
+    //
+    //-----------------------------------------------------------------------------
+    OPENSEA_OPERATIONS_API int pull_FARM_Combined_Log(tDevice *device, const char * const filePath, uint32_t transferSizeBytes, uint32_t delayTime);
 
 #if defined (__cplusplus)
 }
