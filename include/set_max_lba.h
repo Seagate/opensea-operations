b664ccc6f10e9beec7ff04fbeb1bd49df5d34e22
Tyler Erickson <tyler.erickson@seagate.com>
2023-08-02 11:09:05 -0600

    feat: Adding a function to help detect if HBA/bridge/adapter is in sync with drive capacity
    Adding a feature to help detect if the adapter a device is attached to is in sync with the drive's change in capacity or not.
    If a device is behind a SATL then it may not be up to date with the capacity once it has changed. The only known way to resolve this is to reboot the system.
    This function will attempt reading VPD page 89h (SAT info) to see if that causes a refresh, but it is not guaranteed to refresh this information.
    This can help a user determine that they need to do a reboot before they can use the drive at the new capacity.
    
    Signed-off-by: Tyler Erickson <tyler.erickson@seagate.com>

--- include/set_max_lba.h
+++ include/set_max_lba.h
@@ -123,6 +123,24 @@ extern "C"
     //-----------------------------------------------------------------------------
     OPENSEA_OPERATIONS_API int restore_Max_LBA_For_Erase(tDevice* device);
 
+    //-----------------------------------------------------------------------------
+    //
+    //  bool is_Max_LBA_In_Sync_With_Adapter_Or_Driver(tDevice* device)
+    //
+    //! \brief   This function checks if the adapter and device are reporting the same maxLBA to the host.
+    //!          SATA drives behind USB adapters or SAS HBAs work through a SATL (translator) that may not be in sync after changing the maxLBA.
+    //!          This checks if these things are in sync or not and reports the status. If not in sync, a powercycle or reboot is necessary for the adapter
+    //!          to update what it knows about the attached device.
+    //
+    //  Entry:
+    //!   \param[in]  device file descriptor
+    //!
+    //  Exit:
+    //!   \return true = in sync, false = out of sync. Recommend rebooting/power cycling to resync the adapters knowledge of the device.
+    //
+    //-----------------------------------------------------------------------------
+    OPENSEA_OPERATIONS_API bool is_Max_LBA_In_Sync_With_Adapter_Or_Driver(tDevice* device);
+
 #if defined (__cplusplus)
 }
 #endif
