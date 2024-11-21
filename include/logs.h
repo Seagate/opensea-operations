11297b2b1146eaa409b83ee6c0d89e36f039ac99
Ankur Gaikwad <ankur.d.gaikwad@seagate.com>
2024-11-21 12:39:48 +0530

    Changed in the header file

--- include/logs.h
+++ include/logs.h
@@ -528,7 +528,7 @@ extern "C" {
                                                     const char * const filePath, uint32_t transferSizeBytes, char *fileNameUsed);
     
     OPENSEA_OPERATIONS_API eReturnValues pull_FARM_LogPage(tDevice *device, const char * const filePath, uint32_t transferSizeBytes, uint32_t issueFactory, \
-                                                uint16_t logPage, uint8_t logAddress, eLogPullMode mode);
+                                                uint16_t logPage, uint8_t logAddress, eLogPullMode mode, eLogFileNamingConvention FILE_NAME_TYPE);
 
     //-----------------------------------------------------------------------------
     //
